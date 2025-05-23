ba6f7650ea1c3311a5110cffbb19c35690ece25d
Griffin Roberts <gc20roberts@gmail.com>
2025-05-23 12:16:43 -0500

    Add Chips class implementing Food

---
+++ DeliciousApplication/src/main/java/com/pluralsight/models/Chips.java
@@ -0,0 +1,22 @@
+package com.pluralsight.models;
+
+public class Chips implements Food {
+    private String flavor;
+
+    public Chips(String flavor) {
+        this.flavor = flavor;
+    }
+
+    public String getFlavor() {
+        return flavor;
+    }
+
+    public void setFlavor(String flavor) {
+        this.flavor = flavor;
+    }
+
+    @Override
+    public double getPrice() {
+        return 1.5;
+    }
+}
