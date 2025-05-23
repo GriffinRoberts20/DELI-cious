9bcc16be2e271b5655410b362deb512f57bc36b2
Griffin Roberts <gc20roberts@gmail.com>
2025-05-23 12:12:50 -0500

    add Drink class implementing Food

---
+++ DeliciousApplication/src/main/java/com/pluralsight/models/Drink.java
@@ -0,0 +1,37 @@
+package com.pluralsight.models;
+
+public class Drink implements Food{
+    private int size;
+    private String flavor;
+
+    public Drink(String flavor, int size) {
+        this.flavor = flavor;
+        this.size = size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public String getFlavor() {
+        return flavor;
+    }
+
+    public void setFlavor(String flavor) {
+        this.flavor = flavor;
+    }
+
+    @Override
+    public double getPrice() {
+        return switch(size){
+            case 1 -> 2;
+            case 2 -> 2.5;
+            case 3 -> 3;
+            default -> 0;
+        };
+    }
+}
