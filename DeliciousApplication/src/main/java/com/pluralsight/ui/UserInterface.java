82ccebfdb39e897e738c79511bd3e6b04af5308c
Griffin Roberts <gc20roberts@gmail.com>
2025-05-27 10:57:15 -0500

    add order screen

--- DeliciousApplication/src/main/java/com/pluralsight/ui/UserInterface.java
+++ DeliciousApplication/src/main/java/com/pluralsight/ui/UserInterface.java
@@ -1,8 +1,12 @@
 package com.pluralsight.ui;
 
+import com.pluralsight.models.*;
+
 import java.util.Scanner;
 
 public class UserInterface {
+
+
 
     public static void display() throws InterruptedException {
         homeMenu();
@@ -14,21 +18,58 @@ public class UserInterface {
         while(gettingChoice){
             newScreen();
             System.out.println("Home Screen");
-            divider(60);
+            divider(30);
             System.out.println("   1) New Order");
             System.out.println("   0) Exit");
             switch (getChoice("Enter choice: ")) {
                 case "1":
-                    System.out.println("put order menu here");
+                    orderMenu();
                     break;
                 case "0":
                     System.out.println("Closing application.");
                     gettingChoice=false;
                     break;
                 default:
-                    System.out.println("Invalid input");
+                    System.out.println("Invalid input, must be 0-1.");
             }
-            Thread.sleep(3000);
+            if(gettingChoice) Thread.sleep(3000);
+        }
+    }
+
+    public static void orderMenu() throws InterruptedException {
+        Order order=new Order();
+        boolean gettingChoice=true;
+        while(gettingChoice){
+            newScreen();
+            System.out.println("Order Menu");
+            divider(30);
+            System.out.println("   1) Add Sandwich");
+            System.out.println("   2) Add Drink");
+            System.out.println("   3) Add Chips");
+            System.out.println("   4) Checkout");
+            System.out.println("   0) Cancel Order");
+            switch (getChoice("Enter choice: ")){
+                case "1":
+                    System.out.println("put add sandwich here");
+                    break;
+                case "2":
+                    System.out.println("put add drink here");
+                    break;
+                case "3":
+                    System.out.println("put add chips here");
+                    break;
+                case "4":
+                    System.out.println("put checkout here");
+                    break;
+                case "0":
+                    System.out.println("Canceling order.");
+                    gettingChoice=false;
+                    order=new Order();
+                    break;
+                default:
+                    System.out.println("Invalid input, must be 0-4.");
+            }
+            if(gettingChoice) Thread.sleep(3000);
         }
     }
 
