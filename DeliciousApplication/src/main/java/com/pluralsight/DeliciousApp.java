9a21058b010d6c109540794a409d164a6e729a73
Griffin Roberts <gc20roberts@gmail.com>
2025-05-27 13:12:55 -0500

    clean main app and receipts folder

--- DeliciousApplication/src/main/java/com/pluralsight/DeliciousApp.java
+++ DeliciousApplication/src/main/java/com/pluralsight/DeliciousApp.java
@@ -6,22 +6,6 @@ import static com.pluralsight.util.ReceiptWriter.*;
 
 public class DeliciousApp {
     public static void main(String[] args) throws InterruptedException {
-        Order order=new Order();
-        Sandwich sandwich=new Sandwich(true,"White",8);
-        sandwich.addTopping(new Meat("Steak",true));
-        sandwich.addTopping(new Meat("Bacon",false));
-        sandwich.addTopping(new Cheese("American",true));
-        sandwich.addTopping(new Cheese("Cheddar",false));
-        sandwich.addTopping(new Topping("Lettuce",false));
-        sandwich.addTopping(new Topping("Onions",false));
-        sandwich.addTopping(new Topping("Pickles",false));
-        sandwich.addTopping(new Topping("Mayo",false));
-        order.addFood(sandwich);
-        order.addFood(new Drink("Coca Cola",2));
-        order.addFood(new Chips("Sour Cream"));
-        System.out.println(order.getReciept());
-        writeReceipt(order.getReciept());
-
         display();
     }
 }
